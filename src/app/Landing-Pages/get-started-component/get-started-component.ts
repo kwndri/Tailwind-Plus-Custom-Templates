34927f91e2f77314e352cf16f53be891ddab46ba
kwndri <kondritsas14@gmail.com>
2025-09-20 19:55:29 +0300

    Minor fixes and small refactor of the code

--- src/app/Landing-Pages/get-started-component/get-started-component.ts
+++ src/app/Landing-Pages/get-started-component/get-started-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IconsComponent } from '../../icons/icons.component';
+import { LearnMoreComponent } from '../learn-more-component/learn-more-component';
 
 @Component({
   standalone: true,
   selector: 'app-get-started-component',
-  imports: [IconsComponent],
+  imports: [IconsComponent, LearnMoreComponent],
   templateUrl: './get-started-component.html',
   styleUrl: './get-started-component.css',
 })
--- src/app/Landing-Pages/message-component/message-component.ts
+++ src/app/Landing-Pages/message-component/message-component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IconsComponent } from '../../icons/icons.component';
+import { LearnMoreComponent } from '../learn-more-component/learn-more-component';
 
 @Component({
   standalone: true,
   selector: 'app-message-component',
-  imports: [IconsComponent],
+  imports: [IconsComponent, LearnMoreComponent],
   templateUrl: './message-component.html',
   styleUrl: './message-component.css',
 })
