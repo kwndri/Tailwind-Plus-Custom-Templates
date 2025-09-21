ac7ed6e76ed3165edd92a08fea5939294dfe00ec
kwndri <kondritsas14@gmail.com>
2025-09-21 21:07:11 +0300

    Minor fix in UI

--- src/app/Login-Page/login-component/login-component.ts
+++ src/app/Login-Page/login-component/login-component.ts
@@ -11,7 +11,7 @@ import { Router, RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-login-component',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './login-component.html',
   styleUrl: './login-component.css',
 })
