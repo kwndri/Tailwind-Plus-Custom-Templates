537632524cdd6c8c11b56c9d2d426602f22674e6
kwndri <kondritsas14@gmail.com>
2025-09-25 19:34:34 +0300

    Component containg a big responsive image in the page

---
+++ src/app/About-Pages/about-image-component/about-image-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-about-image-component',
+  imports: [],
+  templateUrl: './about-image-component.html',
+  styleUrl: './about-image-component.css',
+})
+export class AboutImageComponent {}
